c4056e38a232703e2a610788cd9c2bb12025ade4
Shayan Hosseini <shayan.hosseiny@gmail.com>
2020-10-24 22:29:51 +0330

    better file names

--- contracts/method.go
+++
@@ -1,5 +0,0 @@
-package contracts
-
-func getFullMethodName(serviceName string, methodName string) string {
-	return "/" + serviceName + "/" + methodName
-}
--- contracts/server.go
+++ contracts/server.go
@@ -7,33 +7,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-// UnaryRPCContract represents a contract for a unary RPC.
-type UnaryRPCContract struct {
-	MethodName     string
-	PreConditions  []Condition
-	PostConditions []Condition
-}
-
-func (u *UnaryRPCContract) validate() error {
-	for _, c := range u.PreConditions {
-		if err := validatePreCondition(c); err != nil {
-			return err
-		}
-	}
-	for _, c := range u.PostConditions {
-		if err := validatePostCondition(c); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// ServiceContract is a contract defined for a gRPC service.
-type ServiceContract struct {
-	ServiceName  string
-	RPCContracts []*UnaryRPCContract
-}
-
 // Logger represents the interface of a required logger.
 type Logger interface {
 	Info(args ...interface{})
